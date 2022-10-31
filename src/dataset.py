83f8dc43ed0cdb3d1a4d17a0895788066d96c305
qinnzou <qzou@whu.edu.cn>
2022-10-31 11:39:20 +0800

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,3 +1,6 @@
+# this code is from https://github.com/knazeri/edge-connect
+# Nazeri, K., Ng, E., Joseph, T., Qureshi, F. Z., & Ebrahimi, M. (2019). Edgeconnect: Generative image inpainting with adversarial edge learning. arXiv preprint arXiv:1901.00212.
+
 import os
 import glob
 
