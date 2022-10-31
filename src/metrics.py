82e2b4ce15e07f2310152e0e75d4e0b18e1c690a
qinnzou <qzou@whu.edu.cn>
2022-10-31 11:47:37 +0800

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -1,4 +1,4 @@
-# this code is released by https://github.com/knazeri/edge-connect
+# this code is from https://github.com/knazeri/edge-connect
 # Nazeri, K., Ng, E., Joseph, T., Qureshi, F. Z., & Ebrahimi, M. (2019). Edgeconnect: Generative image inpainting with adversarial edge learning. arXiv preprint arXiv:1901.00212.
 
 
